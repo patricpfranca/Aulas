b4ca639eb2369617248eadc31a78ea46a2a105b8
Patric Franca <u300024@hubfintech.local>
2017-02-16 10:41:40 -0200

    Arquivos Aula

---
+++ OperadoresLogicos.java
@@ -0,0 +1,13 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		
+		int idade = 21;
+				
+		boolean result = !(idade >= 18);
+		System.out.println(result);
+
+	}
+
+}
---
+++ OperadoresRelacionais.java
@@ -0,0 +1,14 @@
+
+public class OperadoresRelacionais {
+	
+	public static void main(String[] args){
+		
+		int idade = 16;
+		
+		boolean result = (idade != 18);
+		
+		System.out.println(result);
+		
+	}
+
+}
